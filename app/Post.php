369d70b00300aa2ebdf1c061245e0025bcf69c94
Dmitriy <dilife90@gmail.com>
2018-03-03 23:09:54 +0300

    edit Post

--- app/Post.php
+++ app/Post.php
@@ -4,10 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Storage;
 
+/**
+ * @property int user_id
+ * @property mixed image
+ * @property int category_id
+ * @property int status
+ * @property int is_featured
+ */
 class Post extends Model
 {
     use Sluggable;
+
+    const IS_DRAFT = 0;
+    const IS_PUBLIC = 1;
+
+    protected $fillable = ['title', 'content'];
 
     public function category()
     {
@@ -40,5 +53,131 @@ class Post extends Model
                 'source' => 'title'
             ]
         ];
+    }
+
+    /**
+     * @param $fields
+     * @return static
+     */
+    public static function add($fields)
+    {
+        $post = new static();
+        $post->fill($fields);
+        $post->user_id = 1;
+        $post->save();
+
+        return $post;
+    }
+
+    /**
+     * @param $fields
+     */
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function remove()
+    {
+        Storage::delete('uploads/', $this->image);
+        $this->delete();
+    }
+
+    /**
+     * @param $image
+     */
+    public function uploadImage($image)
+    {
+        if ($image == null) {
+            return;
+        }
+        Storage::delete('uploads/', $this->image);
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+
+    }
+
+    public function getImage()
+    {
+        if ($this->image == null) {
+            return '/img/no-image.png';
+        }
+        return '/uploads/' . $this->image;
+    }
+
+    /**
+     * @param $id
+     */
+    public function setCategory($id)
+    {
+        if ($id == null) {
+            return;
+        }
+
+        $this->category_id = $id;
+        $this->save();
+    }
+
+    /**
+     * @param $ids
+     */
+    public function setTags($ids)
+    {
+        if ($ids == null) {
+            return;
+        }
+        $this->tags()->sync($ids);
+    }
+
+    public function setDraft()
+    {
+        $this->status = self::IS_DRAFT;
+        $this->save();
+    }
+
+    public function setPublic()
+    {
+        $this->status = self::IS_PUBLIC;
+        $this->save();
+    }
+
+    /**
+     * @param $value
+     */
+    public function toggleStatus($value)
+    {
+        if ($value == null) {
+            return $this->setDraft();
+        }
+        return $this->setPublic();
+    }
+
+    public function setFeatured()
+    {
+        $this->is_featured = 1;
+        $this->save();
+    }
+
+    public function setStandart()
+    {
+        $this->is_featured = 0;
+        $this->save();
+    }
+
+    /**
+     * @param $value
+     */
+    public function toggleFeatured($value)
+    {
+        if ($value == null) {
+            return $this->setStandart();
+        }
+        return $this->setFeatured();
     }
 }
