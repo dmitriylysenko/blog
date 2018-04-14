9764e0a11b7a463b729136f1da1282650135a0f2
Dmitriy <dilife90@gmail.com>
2018-04-15 00:25:01 +0300

    final #15

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-03-15 19:47:55.
+    * Generated on 2018-04-11 21:09:36.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
